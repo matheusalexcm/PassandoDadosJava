caaab9d6b6b13b7e07243bbc5926fbd8da19af5f
Matheus <matheusalexandrecm@gmail.com>
2020-05-18 22:39:19 -0300

    Initial commit

---
+++ app/src/main/java/com/tads/passardadosjava/SegundaActivity.java
@@ -0,0 +1,33 @@
+package com.tads.passardadosjava;
+
+//Matheus Alexandre - 18/05/2020
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    private TextView textNome, textIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+
+        textNome = findViewById(R.id.textNome);
+        textIdade = findViewById(R.id.textIdade);
+
+        //Recuperar os dados enviados
+
+        Bundle dados = getIntent().getExtras();
+        String nome = dados.getString("nome");
+        int idade = dados.getInt("idade");
+        Usuario usuario = (Usuario) dados.getSerializable("objeto");
+
+        //Configurar valores recuperados
+        textNome.setText(usuario.getEmail());
+        textIdade.setText(String.valueOf(idade));
+    }
+}
